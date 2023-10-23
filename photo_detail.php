e5fb249c32c083a345ecb454d1c8db276b2605b8
Aya Rabih <109382355+ayarabih@users.noreply.github.com>
2023-10-24 00:24:16 +0300

    Update photo_detail.php

--- photo_detail.php
+++ photo_detail.php
@@ -1,8 +1,5 @@
 <?php
- include('./config.php');   //عملنا كونكت بين الداتا بيز والفايل عن طريق فايل تاني 
-
-/// مسار الفولدر+/css/bootstrap.css
-
+ include('./config.php'); 
 
 ?>
 <!DOCTYPE html>
@@ -111,21 +108,11 @@ https://templatemo.com/tm-556-catalog-z
                             <h3 class='tm-text-gray-dark mb-3'>License</h3>
                             <p>$license.</p>
                         </div>" ;
-
     }
 }
     
     // $stmt->execute();
-   
-  
     ?>
-
-
-
-
-
-
-
     <div class="container-fluid tm-container-content tm-mt-60">
         <div class="row mb-4">
             <!-- <h2 class="col-12 tm-text-primary">    $row['photo_title']    </h2> -->
@@ -183,20 +170,11 @@ https://templatemo.com/tm-556-catalog-z
                     </figcaption>                    
                 </figure>
 
-
-
-
-
                 <div class="d-flex justify-content-between tm-text-gray">
                     <span class="tm-text-gray-light">16 Oct 2020</span>
                     <span>12,460 views</span>
                 </div>
             </div>
-
-
-
-
-
 
             <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
                 <figure class="effect-ming tm-video-item">
